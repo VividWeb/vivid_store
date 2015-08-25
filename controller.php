ec61102e8bfa802c719d8fa490bf4883e2bde99c
Michael <michael@isitvivid.com>
2015-08-25 16:55:09 -0400

    clean controller

--- controller.php
+++ controller.php
@@ -1,71 +1,70 @@
 <?php
-namespace Concrete\Package\VividStore;
-use Package;
 
-use \Concrete\Package\VividStore\Src\VividStore\Payment\Method as PaymentMethod;
-use \Concrete\Package\VividStore\Src\VividStore\Shipping\MethodType as ShippingMethodType;
-use \Concrete\Package\VividStore\Src\VividStore\Utilities\Installer;
+namespace Concrete\Package\VividStore;
+
+use Package;
+use Concrete\Package\VividStore\Src\VividStore\Payment\Method as PaymentMethod;
+use Concrete\Package\VividStore\Src\VividStore\Shipping\MethodType as ShippingMethodType;
+use Concrete\Package\VividStore\Src\VividStore\Utilities\Installer;
 use Route;
 
-defined('C5_EXECUTE') or die(_("Access Denied."));
+defined('C5_EXECUTE') or die(_('Access Denied.'));
 
-class Controller extends Package
+class controller extends Package
 {
     protected $pkgHandle = 'vivid_store';
     protected $appVersionRequired = '5.7.3';
     protected $pkgVersion = '3.0dev1';
     protected $pkgAutoloaderRegistries = array(
         'src/AuthorizeNet' => '\AuthorizeNet',
-        'src/Omnipay' => '\Omnipay'
+        'src/Omnipay' => '\Omnipay',
     );
     public function getPackageDescription()
     {
-        return t("Add a Store to your Site");
+        return t('Add a Store to your Site');
     }
 
     public function getPackageName()
     {
-        return t("Vivid Store");
+        return t('Vivid Store');
     }
-	
-	public function installStore()
-	{
-		$pkg = Package::getByHandle('vivid_store');
-		if (version_compare($pkg->getPackageVersion(), '2.1', '<')) {
+
+    public function installStore()
+    {
+        $pkg = Package::getByHandle('vivid_store');
+        if (version_compare($pkg->getPackageVersion(), '2.1', '<')) {
             Installer::renameDatabaseTables($pkg);
         }
-		Installer::installSinglePages($pkg);
-		Installer::installProductParentPage($pkg);
+        Installer::installSinglePages($pkg);
+        Installer::installProductParentPage($pkg);
         Installer::installStoreProductPageType($pkg);
-		Installer::updateConfigStorage($pkg);
-		Installer::setDefaultConfigValues($pkg);
-		Installer::installPaymentMethods($pkg);
-		Installer::installShippingMethods($pkg);
+        Installer::updateConfigStorage($pkg);
+        Installer::setDefaultConfigValues($pkg);
+        Installer::installPaymentMethods($pkg);
+        Installer::installShippingMethods($pkg);
         Installer::installBlocks($pkg);
-		Installer::setPageTypeDefaults($pkg);
-		Installer::installCustomerGroups($pkg);
-		Installer::installUserAttributes($pkg);
-		Installer::installOrderAttributes($pkg);
-		Installer::installProductAttributes($pkg);
-		Installer::createDDFileset($pkg);
-		Installer::installOrderStatuses($pkg);
-	}
+        Installer::setPageTypeDefaults($pkg);
+        Installer::installCustomerGroups($pkg);
+        Installer::installUserAttributes($pkg);
+        Installer::installOrderAttributes($pkg);
+        Installer::installProductAttributes($pkg);
+        Installer::createDDFileset($pkg);
+        Installer::installOrderStatuses($pkg);
+    }
 
     public function install()
     {
         $pkg = parent::install();
-		$this->installStore();
+        $this->installStore();
     }
 
     public function upgrade()
     {
         //as of 7.5.1, the Package::upgrade() method will wipe out our non-ORM tables.
         //parent::upgrade();
-		Installer::upgrade();
-		$this->installStore();				
+        Installer::upgrade();
+        $this->installStore();
     }
-
-    
 
     public function registerRoutes()
     {
@@ -76,9 +75,9 @@ class Controller extends Package
         Route::register('/cart/getmodal', '\Concrete\Package\VividStore\Src\VividStore\Cart\CartModal::getCartModal');
         Route::register('/productmodal', '\Concrete\Package\VividStore\Src\VividStore\Product\ProductModal::getProductModal');
         Route::register('/checkout/getstates', '\Concrete\Package\VividStore\Src\VividStore\Utilities\States::getStateList');
-        Route::register('/checkout/updater','\Concrete\Package\VividStore\Src\VividStore\Utilities\Checkout::updater');
-        Route::register('/productfinder','\Concrete\Package\VividStore\Src\VividStore\Utilities\ProductFinder::getProductMatch');
-        Route::register('/checkout/paypalresponse','\Concrete\Package\VividStore\Src\VividStore\Payment\Methods\PaypalStandard\PaypalStandardPaymentMethod::validateCompletion');
+        Route::register('/checkout/updater', '\Concrete\Package\VividStore\Src\VividStore\Utilities\Checkout::updater');
+        Route::register('/productfinder', '\Concrete\Package\VividStore\Src\VividStore\Utilities\ProductFinder::getProductMatch');
+        Route::register('/checkout/paypalresponse', '\Concrete\Package\VividStore\Src\VividStore\Payment\Methods\PaypalStandard\PaypalStandardPaymentMethod::validateCompletion');
     }
     public function on_start()
     {
@@ -87,22 +86,19 @@ class Controller extends Package
     public function uninstall()
     {
         $authnetpm = PaymentMethod::getByHandle('auth_net');
-        if(is_object($authnetpm)){
+        if (is_object($authnetpm)) {
             $authnetpm->delete();
         }
         $invoicepm = PaymentMethod::getByHandle('invoice');
-        if(is_object($invoicepm)){
+        if (is_object($invoicepm)) {
             $invoicepm->delete();
         }
-		$paypalpm = PaymentMethod::getByHandle('paypal_standard');
-        if(is_object($paypalpm)){
+        $paypalpm = PaymentMethod::getByHandle('paypal_standard');
+        if (is_object($paypalpm)) {
             $paypalpm->delete();
         }
-		$shippingMethodType = ShippingMethodType::getByHandle('flat_rate');
-		$shippingMethodType->delete();
+        $shippingMethodType = ShippingMethodType::getByHandle('flat_rate');
+        $shippingMethodType->delete();
         parent::uninstall();
     }
-
-
 }
-?>
